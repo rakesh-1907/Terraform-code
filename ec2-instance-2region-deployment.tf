1bfa544085094f0ccbfb3dac69c298717527cba0
rakesh patil <patil.rakesh0719@gmail.com>
2025-02-14 11:25:27 +0530

    fixed the code bug

--- ec2-instance-2region-deployment.tf
+++ ec2-instance-2region-deployment.tf
@@ -17,11 +17,21 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
-resource "aws_instance" "Test-Server" {
+resource "aws_instance" "Database-Server" {
     ami = "ami-00bb6a80f01f03502"
     instance_type = "t2.micro"
     tags={
-        name="Test-server"
+        name="Database-server"
+    }
+  
+}
+
+resource "aws_instance" "Singapore-Server" {
+    provider = aws.southeast
+    ami = "ami-039454f12c36e7620"
+    instance_type = "t2.micro"
+    tags={
+        name="Singapore-Server"
     }
   
 }
